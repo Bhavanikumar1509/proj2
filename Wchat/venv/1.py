d2963ae14de5d1b3505542494536b09d982b349a
kalavakunta BHAVANI <86163762+Bhavanikumar1509@users.noreply.github.com>
2021-06-27 20:26:15 +0000

    N

--- Wchat/venv/1.py
+++ Wchat/venv/1.py
@@ -1,1 +1,18 @@
-print ("hello worl")
+from flask import Flask,render_template
+app =Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html',moto='lets chat free')
+    
+@app.route('/about')
+def about():
+    return "<h1> Hello world about page</h1>"
+
+@app.route('/chatroom')
+def chatroom():
+    return "<h1>welcome to my chatroom</h1>"
+    
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=True)
