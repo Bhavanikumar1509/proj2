333e39b7bc5f694f62144598c5a933217adb54ef
kalavakunta BHAVANI <86163762+Bhavanikumar1509@users.noreply.github.com>
2021-09-28 18:54:58 +0000

    database integration

--- Wchat/venv/1.py
+++ Wchat/venv/1.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask, request, render_template 
+
 app =Flask(__name__)
 @app.route('/')
 def index():
@@ -12,8 +13,18 @@ def signup():
     return render_template('signup.html')
 @app.route('/login')
 def login():
-    return render_template('login.html')
-  
+    return render_template('login.html') 
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/', methods =["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+       # getting input with  email in HTML form
+       email = request.form.get("email")
+       # getting input with password in HTML form 
+       password = request.form.get("password") 
+       return "email is "+email + "password"+password
+    return render_template("signup.html") 
   
 
   
@@ -22,3 +33,4 @@ if __name__ == "__main__":
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
     app.run(debug=True)
+    
